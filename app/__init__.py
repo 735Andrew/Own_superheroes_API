d1af8fc4e9c1092d97b77e47f3a1558f87bfceeb
735Andrew <kva6519@gmail.com>
2025-07-14 15:47:47 +0300

    build: Add opportunity for deployment with Docker

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import DATABASE_URL
-
+from time import sleep
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
 
 db = SQLAlchemy(app)
-
+sleep(5)
 
 from app import routes, models
 
